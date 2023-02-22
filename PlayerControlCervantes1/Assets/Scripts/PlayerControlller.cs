4c528f102f16a70ca6e55096d9dfdf3cb05aa725
06jessec <noodleboy77@icloud.com>
2023-02-22 10:00:50 -0800

    Finished Player Control
    We finished doing the whole thing including the bonuses

--- PlayerControlCervantes1/Assets/Scripts/PlayerControlller.cs
+++ PlayerControlCervantes1/Assets/Scripts/PlayerControlller.cs
@@ -8,6 +8,9 @@ public class PlayerControlller : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +24,14 @@ public class PlayerControlller : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         // Moves the car forward based on vertical input 
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       
         // Rotates the car based oon horizontal input 
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+       
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
